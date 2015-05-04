17c4717b9666ab61d38b73405269524e63af7770
Alexadra <alexandra.zhmurko@gmail.com>
2015-05-04 11:30:44 +0300

    added function that returns a

--- gittestjava/src/gittestjava/FirstClass.java
+++ gittestjava/src/gittestjava/FirstClass.java
@@ -1,11 +1,17 @@
 package gittestjava;
 
 public class FirstClass {
+    
+    public static int first(){
+	int a = 1;
+	return a;
+    }
 
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("hey");
 	System.out.print("1");
+	System.out.println(first());
     }
 
 }
