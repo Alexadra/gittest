38209b040ddced713a64f2b5253d6aed000ac411
Alexadra <alexandra.zhmurko@gmail.com>
2015-05-07 01:08:37 +0300

    conflict merge 2

--- gittestjava/src/gittestjava/FirstClass.java
+++ gittestjava/src/gittestjava/FirstClass.java
@@ -10,7 +10,7 @@ public class FirstClass {
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("hey");
-	System.out.print("1");
+	System.out.print("Change for conflict 2");
 	System.out.println(first());
     }
 
